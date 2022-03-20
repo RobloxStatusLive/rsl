02cf4ba114166d33999ac387e88fb68f01efb09b
iiPython <ben@iipython.cf>
2022-03-19 21:25:31 -0500

    Favicon

--- rsl/routes/public.py
+++ rsl/routes/public.py
@@ -2,8 +2,8 @@
 
 # Modules
 import json
-from rsl import app
-from flask import render_template
+from rsl import app, rpath
+from flask import render_template, send_from_directory
 
 # Routes
 @app.route("/")
@@ -16,3 +16,7 @@ def route_index() -> None:
         data = sorted([d for n, d in data.items()], key = lambda d: d["name"]),
         status = ("down", "red") if len(down) > 3 else (("partially down", "yellow") if len(slow) > 3 else ("online", "green"))
     ), 200
+
+@app.route("/s/<path:path>")
+def route_static_file(path: str) -> None:
+    return send_from_directory(rpath("src/static"), path, conditional = True)
