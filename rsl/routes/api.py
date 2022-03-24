3bd10587948a036528ed8151b77bbc91d0548718
iiPython <ben@iipython.cf>
2022-03-24 10:34:59 -0500

    API documentation

--- rsl/routes/api.py
+++ rsl/routes/api.py
@@ -2,9 +2,17 @@
 
 # Modules
 from rsl import app
-from flask import abort, jsonify, request
+from flask import abort, jsonify, request, redirect, render_template
 
 # Routes
+@app.route("/api")
+def route_api_doc_redirect() -> None:
+    return redirect("/api/docs")
+
+@app.route("/api/docs")
+def route_api_docs() -> None:
+    return render_template("api.html"), 200
+
 @app.route("/api/status")
 def route_api_status() -> None:
     data = app.db.get_current()
@@ -15,9 +23,13 @@ def route_api_status() -> None:
 
 @app.route("/api/historical")
 def route_api_historical() -> None:
-    service_data = app.db.get_service_data(request.args.get("id"))
-    if not service_data:
-        return abort(400)
+    try:
+        service_data = app.db.get_service_data(request.args.get("id"))
+        if not service_data:
+            return abort(404)
+
+    except Exception:
+        return abort(404)
 
     return jsonify(
         name = service_data[0],
